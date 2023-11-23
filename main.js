0b20f98bb09070cbde77ec2a20ccfcbdeac6535f
yazmimmm <142514444+yazmimmm@users.noreply.github.com>
2023-11-23 11:00:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,26 @@
 
-
-function tocaSomPom  (idEementoAudio) {
-    document.querySelector(idEementoAudio).play();
-
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
+ 
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-const listaDeTeclas=document.querySelectorAll('.tecla')
-listaDeTeclas[0].onclick=querySelectorAll('.tecla');
-let contador = 0;
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-while (contador < listaDeTeclas.length) {
-   const efeito=listaDeTeclas[contador].classList[1];
-   const idAudio="#som_"+efeito;
-   listaDeTeclas  [contador].onclick=function()
-{
- tocaSom(idAudio);   
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (){
+        tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 }
- contador=contador+1;
- console.log(contador);
-}
-
-
-
-
-
-
-
-//Essa funçao consegue  selecionar  e ler o arquivo, mas nao conseguimos
-//manipular o elemento
-document.querySelector('.tecla_pom');
-
